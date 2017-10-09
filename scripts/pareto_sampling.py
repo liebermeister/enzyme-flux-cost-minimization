67988c13fadb306fc6c0afee8b5086ef0ec514dd
eladnoor <noor@imsb.biol.ethz.ch>
2017-10-09 17:18:05 +0200

    improving Pareto plots

--- scripts/pareto_sampling.py
+++ scripts/pareto_sampling.py
@@ -10,13 +10,12 @@ from prepare_data import read_pareto_zipfile
 import os
 import definitions as D
 import pandas as pd
-import matplotlib.pyplot as plt
 
 PICKLE_FNAME = os.path.join(D.TEMP_DIR, 'pareto_sampling.pkl')
 
 def write_cache_files():
     data_list = []
-    for set_fname in ['n39-p80', 'n39-p81']:
+    for set_fname in ['n39-p1', 'n39-p80', 'n39-p81', 'n39-p82']:
         zip_fname = os.path.join(D.DATA_DIR, set_fname + '.zip')
         data_list.append(read_pareto_zipfile(zip_fname))
     data = pd.concat(data_list, axis=0, join='inner')
@@ -27,29 +26,4 @@ def write_cache_files():
     data.to_pickle(PICKLE_FNAME)
 
 if __name__ == '__main__':
-    if not os.path.exists(PICKLE_FNAME):
-        write_cache_files()
-        
-    data = pd.read_pickle(PICKLE_FNAME)
-
-    fig, ax = plt.subplots(1, 1, figsize=(5, 5)) 
-    
-    xdata = data[D.YIELD_L]
-    ydata = data[D.GROWTH_RATE_L]
-    CS = ax.scatter(xdata, ydata, marker='o',
-                    facecolors=(.8, .7, .7), edgecolors=None, alpha=0.2)
-    ax.set_xlabel(D.YIELD_L)
-    ax.set_ylabel(D.GROWTH_RATE_L)
-
-    # find the EFMs which are on the pareto front and mark them
-    pareto_idx = []
-    for i in ydata.sort_values(ascending=False).index:
-        if pareto_idx == [] or xdata[i] > xdata[pareto_idx[-1]]:
-            pareto_idx.append(i)
-
-    xpareto = xdata[pareto_idx]
-    ypareto = ydata[pareto_idx]
-    ax.plot(xpareto, ypareto, marker='.', linewidth=1, color=(0, 0, 0.5),
-            markersize=15)
-    
-    fig.savefig(os.path.join(D.OUTPUT_DIR, 'Fig_pareto_sampling.pdf'))
+    write_cache_files()
